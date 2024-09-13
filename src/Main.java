bf761e0a647fb423e9298ef093c8ec0f4b8a7c85
Oskar Ray-Frayssinet <oskar.ray.frayssinet@iths.se>
2024-09-13 10:37:57 +0200

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // arraylist för att samla alla personer (Person)
+        ArrayList<Person> allPersons = new ArrayList<>();
+
+        // generera ett visst antal personer och spara i personlistan
+        allPersons.add(new Person("Oskar"));
+        allPersons.add(new Person("Ulf"));
+        allPersons.add(new Person("Jennie"));
+
+        // skriva ut alla personer
+        for(Person p : allPersons) {
+            System.out.println(p);
+        }
+    }
+}
